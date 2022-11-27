aa49ee0099cb63f27efe4a1b3a8c8fb21c1336e2
OlenchenkoVladimir <89122417+OlenchenkoVladimir@users.noreply.github.com>
2022-11-27 21:57:55 +0300

    Basic methods

---
+++ Basic methods/lesson1_step6.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+    
+    check_robot = browser.find_element(By.ID, "robotCheckbox")
+    check_robot.click()
+    
+    radiobutton_robot = browser.find_element(By.ID, "robotsRule")
+    radiobutton_robot.click()
+    
+    sub = browser.find_element(By.CLASS_NAME, "btn-default")
+    sub.click()
+finally:
+    time.sleep(10)
+    browser.quit()
